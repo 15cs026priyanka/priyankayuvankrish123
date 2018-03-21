2258a2017979c60c371986506017f4a752524690
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-21 16:11:50 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,7 @@
+i=int(input("enter the first number")
+j=int(input("enter the second number")
+k=int(input("enter the third number")
+for n in range of numbers:
+a[k]=a[i]+a[j]:
+print"display the all possibilties values"
+return num
